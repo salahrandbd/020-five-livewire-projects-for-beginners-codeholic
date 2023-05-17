d32ac80d514b9c960d342a36ee9ccbff5d80af63
Sourav Malo <sourav.cse5.bu@gmail.com>
2023-05-17 10:19:09 +0600

    calculator project done

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Livewire\Calculator;
+use App\Http\Livewire\Counter;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +15,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', Counter::class)->name('counter');
+Route::get('/calculator', Calculator::class)->name('calculator');
